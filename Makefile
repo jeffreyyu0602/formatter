9b546b3f49b423ae06a14538315b6344ec9f43d9
Jeffrey Yu <jey070@ucsd.edu>
2018-06-16 01:23:56 -0700

    Added comments for program and init target for Makefile.

--- Makefile
+++ Makefile
@@ -1,9 +1,17 @@
-rename:
-	gcc -g -std=c99 main.c
+rename: main.o
+	gcc -o rename main.o
+
+main.o: main.c
+	gcc -c main.c
 
 new:
 	make clean
 	make rename
 
+init:
+	make rename
+	echo "alias rename=`pwd`/rename" >> ~/.bash_profile
+	source ~/.bash_profile
+
 clean:
-	rm -rf a.out a.out.dSYM
+	rm -rf *.o rename
--- main.c
+++ main.c
@@ -5,29 +5,38 @@
 #include <dirent.h>
 #include <stdlib.h>
 
-int debug = 0;
-
+/* Function name: readFiles
+ * Description: this function reads all the filenames from a directory.
+ * Parameters: dirName: the path of the directory.
+ *             filenames: a pointer to an array of string.
+ * Return value: number of filenames read in.
+ */
 int readFiles(const char * dirName, char *** filenames) {
-  DIR * dir;
-  struct dirent * dp;
-  FILE * fp;
-  int size = 0;
+  DIR * dir; /* A pointer to the directory that we are reading files from. */
+  struct dirent * dp; /* A pointer to each file of in the directory. */
+  int size = 0; /* Number of files that we read. */
 
+  /* Open directory and check if the process succeeded. */
   dir = opendir(dirName);
   if (!dir) {
     fprintf(stderr, "Cannot open directory!\n");
     return -1;
   }
 
+  /* Read in each file. */
   while (dp = readdir(dir)) {
+    /* Eliminate current directory and parrent directory. */
     if (!strcmp(dp->d_name, ".") || !strcmp(dp->d_name, ".."))
       continue;
 
+    /* Allocate space for the array. */
     if (size == 0) {
       *filenames = calloc(1, sizeof(void *));
     } else {
       *filenames = realloc(*filenames, (size + 1) * sizeof(void *));
     }
+
+    /* Allocate memory for filename and copy the name. */
     (*filenames)[size] = malloc(BUFSIZ);
     strcpy((*filenames)[size], dp->d_name);
     size++;
@@ -38,20 +47,21 @@ int readFiles(const char * dirName, char *** filenames) {
 
 /*
  * Function name: match
- * Description: This function was a modified version of strcmp. It can
- *              recognize wild card character and store them inside an array
- *              for future use.
+ * Description: This function was a improved version of strcmp. It can
+ *              recognize wild card characters and store replacement inside 
+ *              arrays for future use.
+ * Parameters: str: a pointer to the string that we want to check.
+ *             pattern: a pointer to the pattern string.
+ *             charPtr: a pointer to the char array that stores replacement
+ *                      for wildcard dot.
+ *             strPtr: a pointer to a char array that stores the replacement
+ *                     for wildcard Kleene star.
+ * Return value: 1 indicate that pattern is found.
  */
 int match(char * str, char * pattern, char * charPtr, char * strPtr) {
-  /* Debug message. */
-  if (debug) {
-    fprintf(stderr, "String is %s\n", str);
-    fprintf(stderr, "Pattern is %s\n", pattern);
-  }
-
   /* Loop through each character of pattern. */
   while (*pattern) {
-    /* If meet a wildcard character, store it to charPtr array and increment
+    /* If meet a wildcard dot, store it to charPtr array and increment
        all pointers. */
     if (*pattern == '.') {
       *charPtr++ = *str++;
@@ -59,8 +69,9 @@ int match(char * str, char * pattern, char * charPtr, char * strPtr) {
       continue;
     }
 
-    /* If meet an asterisk, loop through the rest of the characters and call
-       match recursively. */
+    /* If meet a Kleene star, loop through the rest of the characters and call
+       match recursively until the pattern is found or we reach the end of the
+       string. Store the replacement string then. */
     if (*pattern == '*') {
       int result; /* Store the result of recursive call. */
       char * start = str; /* Store the original position. */
@@ -69,50 +80,64 @@ int match(char * str, char * pattern, char * charPtr, char * strPtr) {
         result = match(str, pattern, charPtr, strPtr + (str - start) + 1);
         if (result) {
           strncpy(strPtr, start, str - start);
-          break;
+          return 1;
         }
         str++;
       }
-
-      /* Debug message. */
-      if (debug) {
-        fprintf(stderr, "Wild characters are %s\n", charPtr);
-        fprintf(stderr, "Wild strings are %s\n", strPtr);
-      }
-      return result;
     }
 
+    /* Return 0 if meet an unmatched character. */
     if (*str++ != *pattern++) {
       return 0;
     }
   }
-
   return 1;
 }
 
+/* Function name: generateNewName
+ * Description: This function generates a new name based on the pattern found
+ *              in the old filename and replacement.
+ * Parameters: filename: a pointer to the filename string.
+ *             newname: a pointer to a empty char array which stores the new
+ *                      name.
+ *             pattern: a pointer to the pattern string.
+ *             replacement: a pointer to the replacement string.
+ * Return value: none.
+ */
 void generateNewName(char * filename, char * newname, char * pattern,
-                    char * replacement) {
+                     char * replacement) {
+  char * charPtr; /* A pointer that points to wildcard buffer. */
+  char * strPtr; /* A pointer that points to wildstr buffer. */
+  char * rplPtr; /* A pointer to the replacement. */
+  char wildchar[BUFSIZ]; /* A buffer that stores all the replacement for
+                            wildcard dot(.). */
+  char wildstr[BUFSIZ]; /* A buffer that stores all the replacement for wildcard
+                           Kleene star(*). */
+
   while (*filename) {
-    char wildchar[BUFSIZ] = {0};
-    char wildstr[BUFSIZ] = {0};
-    char * charPtr = wildchar;
-    char * strPtr = wildstr;
+    /* Fill array with 0 for convenience. */
+    memset(wildchar, 0, BUFSIZ);
+    memset(wildstr, 0, BUFSIZ);
     if (match(filename, pattern, wildchar, wildstr)) {
-      /* Pattern found. */
-      int rpl_idx = 0;
-      int wildcard_idx = 0;
-      while (replacement[rpl_idx]) {
-        if (replacement[rpl_idx] == '.') {
+      /* Reset pointers to the begining of corresponding arrays. */
+      charPtr = wildchar;
+      strPtr = wildstr;
+      rplPtr = replacement;
+
+      /* Loop through each character of replacement and substitute wildcard
+         with letters in original filename. */
+      while (*rplPtr) {
+        if (*rplPtr == '.') {
           *newname++ = *charPtr++;
-          rpl_idx++;
-        } else if (replacement[rpl_idx] == '*') {
+          rplPtr++;
+        } else if (*rplPtr == '*') {
           strcpy(newname, strPtr);
           filename += strlen(strPtr) - 1;
           newname += strlen(strPtr);
           strPtr = strchr(strPtr, 0) + 1;
-          rpl_idx++;
+          rplPtr++;
         } else {
-          *newname++ = replacement[rpl_idx++];
+          *newname++ = *rplPtr++;
         }
       }
       filename += strlen(pattern);
@@ -123,19 +148,29 @@ void generateNewName(char * filename, char * newname, char * pattern,
   }
 }
 
-
+/*
+ * Function name: main
+ * Description: This is the main driver of this program. It parse command line
+ *              argument from the user and prompt the user to enter pattern and
+ *              replacement. Then it called generateNewName to get the new name
+ *              of each file in a loop.
+ * Parameters: argc: the number of argument.
+ *             argv: the actual argument stored in an array of char pointer.
+ * Return value: 0 on success.
+ */
 int main(int argc, char ** argv) {
+  char buffer[BUFSIZ]; /* Used to store path and new name. */
+  char pattern[BUFSIZ]; /* Store patterns string that the user enters. */
+  char replacement[BUFSIZ]; /* Store replacement string that the user enters. */
+  int opt; /* Command line argument. */
+  int size; /* Number of files in a directory. */
+  static int debug = 0; /* Debug mode. */
+  char ** filenames = 0; /* An array of string that stores all the filenames. */
 
-  char buffer[BUFSIZ];
-  char pattern[BUFSIZ];
-  char replacement[BUFSIZ];
-  int opt;
-  int size;
-  int test = 0;
-  char ** filenames = 0;
+  getcwd(buffer, BUFSIZ); /* Default current directory. */
 
-  getcwd(buffer, BUFSIZ);
-  while ((opt = getopt(argc, argv, "d:xt")) != -1) {
+  /* Parse command line argument. */
+  while ((opt = getopt(argc, argv, "d:x")) != -1) {
     switch (opt) {
       case 'd':
         strcpy(buffer, optarg);
@@ -143,10 +178,7 @@ int main(int argc, char ** argv) {
       case 'x':
         debug = 1;
         break;
-      case 't':
-        test = 1;
-        break;
-      default:
+      case '?':
         return EXIT_FAILURE;
     }
   }
@@ -171,7 +203,7 @@ int main(int argc, char ** argv) {
       generateNewName(filenames[i], buffer, pattern, replacement);
       if (strcmp(filenames[i], buffer)) {
           fprintf(stderr, "%s\n",buffer);
-          if (!test)
+          if (!debug)
             rename(filenames[i], buffer);
           strcpy(filenames[i], buffer);
       }
